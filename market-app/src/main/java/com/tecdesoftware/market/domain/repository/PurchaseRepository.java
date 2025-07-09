3c136a35aaeea30764add8c3709b4bfc9f879fae
Darío González <dar007alex@gmail.com>
2025-07-09 08:09:12 -0600

    Purchase Classes

---
+++ market-app/src/main/java/com/tecdesoftware/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.market.domain.repository;
+
+import ch.qos.logback.core.net.server.Client;
+import com.tecdesoftware.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> findAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
